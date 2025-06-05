6fbaac2d9b00ca31b9556d96837298d9abd78535
_umannn <134380467+luthfan1234@users.noreply.github.com>
2025-06-05 10:13:12 +0700

    Fix All

--- flask_app/telegram_bot.py
+++ flask_app/telegram_bot.py
@@ -10,8 +10,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Konfigurasi Bot
-BOT_TOKEN = '7718703590:AAGcUryDq5evkLJ6dLiGfMiobACKMPO1csE'  # Ganti dengan token bot Anda
-CHAT_ID = '1193580325'  # Ganti dengan chat ID tujuan
+BOT_TOKEN = ''  # Ganti dengan token bot Anda
+CHAT_ID = ''  # Ganti dengan chat ID tujuan
 BASE_URL = f'https://api.telegram.org/bot{BOT_TOKEN}'
 
 # Interval default
